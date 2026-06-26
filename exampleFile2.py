916aa0f0709022a52ce9906d1103a8e49601851c
onehundredcw-star <onehundredcw@gmail.com>
2026-06-26 14:26:52 -0400

    Played around with for loops

--- exampleFile2.py
+++ exampleFile2.py
@@ -1,1 +1,62 @@
-print("This is my second assignment")
+
+# variable = input("this is an input that will take a response ")
+# print(variable)
+
+# height = int(input("How tall are you in inches? "))
+# if height >= 48:
+#     print("You may ride")
+# else:
+#     print("Sorry kid, get lost")
+
+# password = "bluepen"
+# userLoginInput = input("Enter password: ")
+# if userLoginInput == password:
+#     print("Login successful!")
+# else:
+#     print("Incorrect password.")
+
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print("The number is even.")
+# else:
+#     print("The number is odd.")
+
+# if number % 2 != 0:
+#     print("The number is odd.")
+# else:
+#     print("The number is even.")
+
+# for number in  range(1, 153):
+#     print(number)
+
+# for number in range(10, 0, -1):
+#     print(number)
+# print("Blast off!")
+
+# numberOfRootBeers = int(input("How many bottles of root beer are on the wall? "))
+# for i in range(numberOfRootBeers, 1, -1):
+#     print(f"{i} bottles of root beer on the wall, {i} bottles of root beer. Take one down and pass it around, {i-1} bottles of root beer on the wall.")
+
+total = 0
+for number in range(1, 428):
+    total += number
+print(f"The total sum of numbers from 1 to 427 is: {total}")
+
+while userLoginInput != password;
+    print("Wrong password, try again.")
+    userLoginInput = input("Enter password: ")
+print("Login successful!")
+
+numberList = [1, 4, 5, 4235, 42, -1]
+largestNumber = numberList[0]
+print(largestNumber)
+for number in numberList:
+    if number > largestNumber:
+        largestNumber = number
+print(f"The largest number in the list is: {largestNumber}")
+
+secretNumber = 7
+guess = 0
+while guess != secretNumber:
+    guess = int(input("Guess the secret number: "))
+print("You got it!")
--- hello.py
+++ hello.py
@@ -1,91 +1,110 @@
-print("Hello, World!")
-print("My name is Chris")
+# print("Hello, World!")
+# print("My name is Chris")
 
-# ctrl+/ is how we make a comment
-learner = "Chris"
-print(learner)
+# # ctrl+/ is how we make a comment
+# learner = "Chris"
+# print(learner)
 
-ideal_number_of_pets = 2
-print(ideal_number_of_pets)
+# ideal_number_of_pets = 2
+# print(ideal_number_of_pets)
 
-# this is a float variable
-how_much_is_a_banana = 9.99
-print(how_much_is_a_banana)
+# # this is a float variable
+# how_much_is_a_banana = 9.99
+# print(how_much_is_a_banana)
 
-# this is a boolean variable
-are_our_pets_vaccinated = True
-print(are_our_pets_vaccinated)
+# # this is a boolean variable
+# are_our_pets_vaccinated = True
+# print(are_our_pets_vaccinated)
 
-# print(type(learner))
-# print(type(ideal_number_of_pets))
-# print(type(how_much_is_a_banana))
-# print(type(are_our_pets_vaccinated))
+# # print(type(learner))
+# # print(type(ideal_number_of_pets))
+# # print(type(how_much_is_a_banana))
+# # print(type(are_our_pets_vaccinated))
 
-# is_it_a_number = 333
-# is_this_a_number = "333"
+# # is_it_a_number = 333
+# # is_this_a_number = "333"
 
-# print(type(is_it_a_number))
-# print(type(is_this_a_number))
+# # print(type(is_it_a_number))
+# # print(type(is_this_a_number))
 
-# print(5+3)
-# print(5*3)
-# print(5/3)
+# # print(5+3)
+# # print(5*3)
+# # print(5/3)
 
-# example of modulo
-print(10%3)
-print(10/3)
+# # example of modulo
+# print(10%3)
+# print(10/3)
 
-# example of exponential
-print(2**5)
-print(2**2)
-print(32**73)
+# # example of exponential
+# print(2**5)
+# print(2**2)
+# print(32**73)
 
-# find the area of a rectangle
-# length = input("What is the length of the rectangle? ")
-# length = int(input("What is the length of the rectangle? "))
-# # width = 20
-# width = int(input("What is the width of the rectangle? "))
+# # find the area of a rectangle
+# # length = input("What is the length of the rectangle? ")
+# # length = int(input("What is the length of the rectangle? "))
+# # # width = 20
+# # width = int(input("What is the width of the rectangle? "))
 
-# area = length * width
+# # area = length * width
 
-# print(area)
+# # print(area)
 
-# # find the tax amount
-price = 10
-tax = price * 0.08
+# # # find the tax amount
+# price = 10
+# tax = price * 0.08
 
-print(f"The tax amount is {tax}")
+# print(f"The tax amount is {tax}")
 
-# # find the average
-avg = (10+10+10)/3
+# # # find the average
+# avg = (10+10+10)/3
 
-print(f"The average is {avg}")
+# print(f"The average is {avg}")
 
-# ask the user their favorite color
-fav_color = input("what is your favorite color? ")
+# # ask the user their favorite color
+# fav_color = input("what is your favorite color? ")
 
-# print(fav_color)
-print(f"Your favorite color is {fav_color}")
+# # print(fav_color)
+# print(f"Your favorite color is {fav_color}")
 
-# test multiple inputs
-print("This","is","what","they","are","talking","about")
+# # test multiple inputs
+# print("This","is","what","they","are","talking","about")
 
-print(12, 24, -2, sep=':')
-print('but','not','including', sep='**')
+# print(12, 24, -2, sep=':')
+# print('but','not','including', sep='**')
 
-# create a receipt
-# customer name
-# the price of what they purchased
-# the quantity of the product they purchased
-# the total cost
-customerName = input("Enter customer's name:")
-itemPrice = int(input("what is the cost of the item? "))
-quantity = int(input("how many of the item did they purchase? "))
-totalCost = itemPrice * quantity
-roundedCost = round(totalCost, 2)
-print("Receipt")
-print(f"Customer Name: {customerName}")
-print(f"Item price: ${itemPrice}")
-print(f"quantity: {quantity}")
-print(f"Total cost: ${totalCost}")
-print(f"Rounded cost: ${roundedCost}")
+# # create a receipt
+# # customer name
+# # the price of what they purchased
+# # the quantity of the product they purchased
+# # the total cost
+# customerName = input("Enter customer's name:")
+# itemPrice = int(input("what is the cost of the item? "))
+# quantity = int(input("how many of the item did they purchase? "))
+# totalCost = itemPrice * quantity
+# roundedCost = round(totalCost, 2)
+# print("Receipt")
+# print(f"Customer Name: {customerName}")
+# print(f"Item price: ${itemPrice}")
+# print(f"quantity: {quantity}")
+# print(f"Total cost: ${totalCost}")
+# print(f"Rounded cost: ${roundedCost}")
+
+# # also in the slides
+# print(string1[6])
+# print(string1[3:1])
+# print(string1*2)
+# print(string1.strip())
+# print(string1.split(' '))
+# print(string1.replace('l','s'))
+# print(string1+'Everyone')
+
+# string2 = 40000
+# print(type(str(string2)))
+# print(type(string2))
+
+print("Floating point into String: ", MyStringTwo)
+
+number_list = [1, 2, 3, 4, 5]
+print(boolean_list)
+
